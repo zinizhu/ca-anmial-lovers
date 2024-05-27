534edfc380a2b8c6400128f13e3bc96e502dfaae
Zini Zhu <zinizhu@Zinis-MacBook-Pro.local>
2024-05-27 09:43:26 -0700

    feat: setup router

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route, useNavigate } from "react-router-dom";
+
+const Home = () => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+      navigate("/info");
+  }
+  return  (
+    <>
+    This is the home page.
+    <button onClick={handleClick}>info</button> 
+  </>
+  )
+}
+
+const Info = () => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+      navigate("/");
+  }
+  return (
+    <>
+    This is the info page.
+    <button onClick={handleClick}>back</button> 
+    </>
+  )
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/info" element={<Info />} />
+    </Routes>
   );
 }
 
