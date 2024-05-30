f39767561da360ef29924da7d71820b1700cb37a
Zini Zhu <zinizhu@Zinis-MacBook-Pro.local>
2024-05-29 22:08:23 -0700

    feat: add mock API endpoints

---
+++ backend/src/mock-data/dogs.ts
@@ -0,0 +1,56 @@
+export const DOGS = [
+    {
+        id: 1,
+        name: "Chichi",
+        image: "../images/Chichi.jpeg",
+        age: 2,
+        deadline: "05/02",
+        breed: "Huskey",
+        gender: "Male",
+        weight: 60,
+        medicalCondition: "Needs Care",
+      },
+      {
+        id: 2,
+        name: "Levi",
+        image: "../images/Levi.jpeg",
+        age: 3,
+        deadline: "05/02",
+        breed: "GSD",
+        gender: "Male",
+        weight: 50,
+        medicalCondition: "Needs Care",
+      },
+      {
+        id: 3,
+        name: "Claire",
+        image: "../images/Claire.jpeg",
+        age: 7,
+        deadline: "05/03",
+        breed: "GSD",
+        gender: "Female",
+        weight: 40,
+        medicalCondition: "Good",
+      }
+]
+
+export const DOGS_STATUS = [
+  {
+      id: 1,
+      name: "Chichi",
+      status: "NEW",
+      state: "RESCUE TAGGED"
+    },
+    {
+      id: 2,
+      name: "Levi",
+      status: "IN_PROGRESS",
+      state: "OWNER COMMITTED",
+    },
+    {
+      id: 3,
+      name: "Claire",
+      status: "CLOSED",
+      state: "ADOPTED"
+    }
+]
