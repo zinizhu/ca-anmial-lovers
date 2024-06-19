37b1ca76d520fd3e1e9e3d224b34103a3938b2fc
Bingqing Zu <zubq@MacBook-Air.local>
2024-06-19 16:29:12 -0700

    feat: add sign up form content

--- src/SignUpForm.tsx
+++ src/SignUpForm.tsx
@@ -3,12 +3,16 @@ import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid"; // Grid version 1
-import { Card, CardContent } from "@mui/material";
+import { Card, CardContent, Stack } from "@mui/material";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
 import Divider from "@mui/material/Divider";
-import Autocomplete from "@mui/material/Autocomplete";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import { SelectChangeEvent } from "@mui/material/Select";
+import InputLabel from "@mui/material/InputLabel";
 
 import { useParams } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
@@ -17,10 +21,40 @@ import { DOGS_INFO } from "./Constants";
 import { Header } from "./Header";
 
 export function SignUpForm() {
-  const { id } = useParams();
   const navigate = useNavigate();
 
+  const { id } = useParams();
   const dogInfo = DOGS_INFO.find((dog) => dog.id.toString() == id);
+
+  const [fosterOrAdopt, setFosterOrAdopt] = React.useState("");
+  const handleFosterOrAdoptChange = (event: SelectChangeEvent<string>) => {
+    setFosterOrAdopt(event.target.value as string);
+  };
+
+  const [numberOfCats, setNumberOfCats] = React.useState("");
+  const handleNumberOfCatsChange = (event: SelectChangeEvent<string>) => {
+    setNumberOfCats(event.target.value as string);
+  };
+
+  const [numberOfDogs, setNumberOfDogs] = React.useState("");
+  const handleNumberOfDogsChange = (event: SelectChangeEvent<string>) => {
+    setNumberOfDogs(event.target.value as string);
+  };
+
+  const [homeType, sethomeType] = React.useState("");
+  const handleHomeTypeChange = (event: SelectChangeEvent<string>) => {
+    sethomeType(event.target.value as string);
+  };
+
+  const [yard, setYard] = React.useState("");
+  const handleYardChange = (event: SelectChangeEvent<string>) => {
+    setYard(event.target.value as string);
+  };
+
+  const [experience, setExperience] = React.useState("");
+  const handleExperienceChange = (event: SelectChangeEvent<string>) => {
+    setExperience(event.target.value as string);
+  };
 
   return (
     <>
@@ -85,26 +119,163 @@ export function SignUpForm() {
                     <TextField label="Email" fullWidth margin="normal" />
                   </Grid>
                   <Grid item xs={12} sm={6}>
-                    <TextField label="Phone Number" fullWidth margin="normal" />
-                  </Grid>
-                  <Grid item xs={12} sm={6}>
-                    <TextField label="Address" fullWidth margin="normal" />
-                  </Grid>
-                  <Grid item xs={12} sm={6}>
-                    <TextField label="Home Style" fullWidth margin="normal" />
-                  </Grid>
-                  <Grid item xs={12} sm={6}>
                     <TextField
-                      label="Number of Cats at Home"
+                      required
+                      label="Phone Number"
                       fullWidth
                       margin="normal"
                     />
                   </Grid>
                   <Grid item xs={12} sm={6}>
+                    <TextField label="City" fullWidth margin="normal" />
+                  </Grid>
+                  <Grid item xs={12} sm={6}>
+                    <TextField label="State" fullWidth margin="normal" />
+                  </Grid>
+                  <Grid item xs={12} sm={6}>
+                    <TextField label="Zip Code" fullWidth margin="normal" />
+                  </Grid>
+                  <Grid item xs={12} sm={3} container alignItems="center">
+                    <Typography variant="body1">Foster or Adopt?</Typography>
+                  </Grid>
+                  <Grid item xs={12} sm={9}>
+                    <FormControl
+                      variant="standard"
+                      sx={{ m: 1, minWidth: 240 }}
+                    >
+                      <InputLabel>Foster/Adpot</InputLabel>
+                      <Select
+                        value={fosterOrAdopt}
+                        onChange={handleFosterOrAdoptChange}
+                        label="Foster or Adopt"
+                      >
+                        <MenuItem value={"Foster"}>Foster</MenuItem>
+                        <MenuItem value={"Adopt"}>Adopt</MenuItem>
+                      </Select>
+                    </FormControl>
+                  </Grid>
+                  <Grid item xs={12} sm={3} container alignItems="center">
+                    <Typography variant="body1">Home Type?</Typography>
+                  </Grid>
+                  <Grid item xs={12} sm={3}>
+                    <FormControl
+                      variant="standard"
+                      sx={{ m: 1, minWidth: 240 }}
+                    >
+                      <InputLabel>Home Type</InputLabel>
+                      <Select
+                        value={homeType}
+                        onChange={handleHomeTypeChange}
+                        label="Home Type"
+                      >
+                        <MenuItem value={"Apartment"}>Apartment</MenuItem>
+                        <MenuItem value={"Townhouse or Condo"}>
+                          Townhouse or Condo
+                        </MenuItem>
+                        <MenuItem value={"Single Family House"}>
+                          Single Family House
+                        </MenuItem>
+                      </Select>
+                    </FormControl>
+                  </Grid>
+                  <Grid item xs={12} sm={2} container alignItems="center">
+                    <Typography variant="body1">Has yard?</Typography>
+                  </Grid>
+                  <Grid item xs={12} sm={4}>
+                    <FormControl
+                      variant="standard"
+                      sx={{ m: 1, minWidth: 240 }}
+                    >
+                      <InputLabel>Has Yard</InputLabel>
+                      <Select
+                        value={yard}
+                        onChange={handleYardChange}
+                        label="Yard"
+                      >
+                        <MenuItem value={1}>Yes</MenuItem>
+                        <MenuItem value={0}>No</MenuItem>
+                      </Select>
+                    </FormControl>
+                  </Grid>
+                  <Grid item xs={12} sm={3} container alignItems="center">
+                    <Typography variant="body1">
+                      Number of Cats at Home?
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={12} sm={9}>
+                    <FormControl
+                      variant="standard"
+                      sx={{ m: 1, minWidth: 240 }}
+                    >
+                      <InputLabel>Number of Cats</InputLabel>
+                      <Select
+                        value={numberOfCats}
+                        onChange={handleNumberOfCatsChange}
+                        label="Number of Cats"
+                      >
+                        <MenuItem value={0}>0</MenuItem>
+                        <MenuItem value={1}>1</MenuItem>
+                        <MenuItem value={2}>2</MenuItem>
+                        <MenuItem value={"more than 2"}>more than 2</MenuItem>
+                      </Select>
+                    </FormControl>
+                  </Grid>
+                  <Grid item xs={12} sm={3} container alignItems="center">
+                    <Typography variant="body1">
+                      Number of Dogs at Home?
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={12} sm={9}>
+                    <FormControl
+                      variant="standard"
+                      sx={{ m: 1, minWidth: 240 }}
+                    >
+                      <InputLabel>Number of Dogs</InputLabel>
+                      <Select
+                        value={numberOfDogs}
+                        onChange={handleNumberOfDogsChange}
+                        label="Number of Dogs"
+                      >
+                        <MenuItem value={0}>0</MenuItem>
+                        <MenuItem value={1}>1</MenuItem>
+                        <MenuItem value={2}>2</MenuItem>
+                        <MenuItem value={"more than 2"}>more than 2</MenuItem>
+                      </Select>
+                    </FormControl>
+                  </Grid>
+                  <Grid item xs={12} sm={3} container alignItems="center">
+                    <Typography variant="body1">
+                      Have experience in handling dogs?
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={12} sm={9}>
+                    <FormControl
+                      variant="standard"
+                      sx={{ m: 1, minWidth: 240 }}
+                    >
+                      <InputLabel>Experience in handling dog </InputLabel>
+                      <Select
+                        value={experience}
+                        onChange={handleExperienceChange}
+                        label="Experience"
+                      >
+                        <MenuItem value={1}>Yes</MenuItem>
+                        <MenuItem value={0}>No</MenuItem>
+                      </Select>
+                    </FormControl>
+                  </Grid>
+                  <Grid item xs={12} sm={3} container alignItems="center">
+                    <Typography variant="body1">
+                      Other Preferences/concers/comments?
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={12} sm={9}>
                     <TextField
-                      label="Number of Dogs at Home"
+                      id="outlined-multiline-static"
+                      label="Comments"
+                      multiline
                       fullWidth
-                      margin="normal"
+                      rows={4}
                     />
                   </Grid>
                 </Grid>
